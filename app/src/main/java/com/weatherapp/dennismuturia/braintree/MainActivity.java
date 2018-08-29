2b09090adf744b9d96a124bbee07ce454541f501
dennismuturia <dmuturia2922@gmail.com>
2018-08-29 14:17:09 +0300

    Str the access toklen to empt so I don't think that it will able to log data from the server but alright.

--- app/src/main/java/com/weatherapp/dennismuturia/braintree/MainActivity.java
+++ app/src/main/java/com/weatherapp/dennismuturia/braintree/MainActivity.java
@@ -1,13 +1,128 @@
 package com.weatherapp.dennismuturia.braintree;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.android.volley.toolbox.HttpResponse;
+import com.braintreepayments.api.dropin.DropInActivity;
+import com.braintreepayments.api.dropin.DropInResult;
+import com.braintreepayments.api.interfaces.HttpResponseCallback;
+import com.braintreepayments.api.internal.HttpClient;
+import com.braintreepayments.api.models.PaymentMethodNonce;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 123;
+    final String API_GET_TOKEN = "";
+    final String API_CHECK_OUT = "";
+
+    String token, amount;
+    HashMap<String, String> paramsHash;
+
+    Button btn_pay;
+    EditText edt_amount;
+    LinearLayout group_waiting, group_payments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Activate all the views
+        group_payments = (LinearLayout) findViewById(R.id.payment_group);
+        group_waiting = (LinearLayout) findViewById(R.id.waiting_group);
+        btn_pay = (Button)findViewById(R.id.btn_pay);
+        edt_amount = (EditText)findViewById(R.id.edt_amount);
+
+        //get the method to get the token
+        new getToken().execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE){
+            if (requestCode == RESULT_OK){
+                DropInResult result = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
+                PaymentMethodNonce nonce = result.getPaymentMethodNonce();
+                String strnonce = nonce.getNonce();
+
+
+                if(!edt_amount.getText().toString().isEmpty()){
+                    amount = edt_amount.getText().toString();
+                    paramsHash = new HashMap<>();
+                    paramsHash.put("amount", amount);
+                    paramsHash.put("nonce", strnonce);
+
+
+                    //Send the payments
+                    sendPayments();
+                }else {
+                    Toast.makeText(this, "Enter valid information", Toast.LENGTH_SHORT).show();
+                }
+            }else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "You have cancelled the transaction", Toast.LENGTH_SHORT).show();
+            }else{
+                Exception error = (Exception)data.getSerializableExtra(DropInActivity.EXTRA_ERROR);
+                Log.d("EDMT_ERROR", error.toString());
+            }
+        }
+    }
+
+    private void sendPayments() {
+    }
+
+    private class getToken extends AsyncTask{
+        //Initializer progress dialog
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this, android.R.style.Theme_DeviceDefault_Dialog);
+            progressDialog.setCancelable(true);
+            progressDialog.setMessage("Wait as we contact the server");
+            progressDialog.show();
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            HttpClient client = new HttpClient();
+            client.get(API_GET_TOKEN, new HttpResponseCallback() {
+                @Override
+                public void success(String responseBody) {
+                    //Hide the group waiting
+                    group_waiting.setVisibility(View.GONE);
+                    //Show the group payment
+                    group_payments.setVisibility(View.VISIBLE);
+
+                    //Setting token
+                    token = responseBody;
+                }
+
+                @Override
+                public void failure(Exception exception) {
+                    Log.d("EDMT_ERROR", exception.toString());
+                }
+            });
+            return null;
+        }
+
+        //INitialize the post Execute
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            progressDialog.dismiss();
+        }
     }
 }
